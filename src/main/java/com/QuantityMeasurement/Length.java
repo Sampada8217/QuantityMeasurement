32a185e191432f27c5413fb8de5a97ba4e26dc23
sampada8217 <sampadatirthkar@gmail.com>
2020-06-04 00:49:11 +0530

    Added Code and Tests for Comparison Check

--- src/main/java/com/QuantityMeasurement/Length.java
+++ src/main/java/com/QuantityMeasurement/Length.java
@@ -3,6 +3,10 @@ package com.QuantityMeasurement;
 import java.util.Objects;
 
 public class Length {
+
+
+    private static final double FEET_TO_INCH = 12;
+
     enum Unit{FEET,INCH};
 
     private final Unit unit;
@@ -11,6 +15,14 @@ public class Length {
     public Length(Unit unit,double value) {
         this.unit=unit;
         this.value=value;
+    }
+    public boolean compare(Length thatLength){
+      if(this.unit.equals(Unit.FEET) && thatLength.unit.equals(Unit.FEET))
+          return Double.compare(this.value,thatLength.value)==0;
+      if(this.unit.equals(Unit.FEET) && thatLength.unit.equals(Unit.INCH))
+            return Double.compare(this.value*FEET_TO_INCH,thatLength.value)==0;
+
+      return false;
     }
 
     @Override
--- src/test/java/com/QuantityMeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/QuantityMeasurement/QuantityMeasurementTest.java
@@ -7,38 +7,62 @@ public class QuantityMeasurementTest {
 
     @Test
     public void given0FeetAnd0Feet_shouldReturnEqual() {
-        Length feet1= new Length(Length.Unit.FEET,0.0);
-        Length feet2=new Length(Length.Unit.FEET,0.0);
-        Assert.assertEquals(feet1,feet2);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 0.0);
+        Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAnd1Feet_shouldReturnNotEquals() {
-        Length feet1= new Length(Length.Unit.FEET,0.0);
-        Length feet2=new Length(Length.Unit.FEET,1.0);
-        Assert.assertNotEquals(feet1,feet2);
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET, 1.0);
+        Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void given0InchAnd0Inch_shouldReturnEqual() {
-        Length inch1=new Length(Length.Unit.INCH,0.0);
-        Length inch2=new Length(Length.Unit.INCH,0.0);
-        Assert.assertEquals(inch1,inch2);
+        Length inch1 = new Length(Length.Unit.INCH, 0.0);
+        Length inch2 = new Length(Length.Unit.INCH, 0.0);
+        Assert.assertEquals(inch1, inch2);
 
     }
+
     @Test
     public void given0InchAnd1Inch_shouldReturnEqual() {
-        Length inch1=new Length(Length.Unit.INCH,0.0);
-        Length inch2=new Length(Length.Unit.INCH,1.0);
-        Assert.assertNotEquals(inch1,inch2);
+        Length inch1 = new Length(Length.Unit.INCH, 0.0);
+        Length inch2 = new Length(Length.Unit.INCH, 1.0);
+        Assert.assertNotEquals(inch1, inch2);
 
     }
 
     @Test
     public void given1FeetAnd1Inch_shouldReturnNotEquals() {
-        Length feet1= new Length(Length.Unit.FEET,1.0);
-        Length inch1=new Length(Length.Unit.INCH,1.0);
-        Assert.assertNotEquals(feet1,inch1);
+        Length feet1 = new Length(Length.Unit.FEET, 1.0);
+        Length inch1 = new Length(Length.Unit.INCH, 1.0);
+        Assert.assertNotEquals(feet1, inch1);
+    }
+
+    @Test
+    public void given0FeetAnd0Inch_shouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length inch1 = new Length(Length.Unit.INCH, 0.0);
+        boolean compareCheck = feet1.compare(inch1);
+        Assert.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given0FeetAnd1Inch_shouldReturnNotEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length inch1 = new Length(Length.Unit.INCH, 1.0);
+        boolean compareCheck = feet1.compare(inch1);
+        Assert.assertFalse(compareCheck);
+    }
+
+    @Test
+    public void given1FeetAnd1Feet_shouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET, 1.0);
+        Length feet2= new Length(Length.Unit.FEET, 1.0);
+        boolean compareCheck = feet1.compare(feet2);
+        Assert.assertTrue(compareCheck);
     }
 }
-
